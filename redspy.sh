9b16993c67e16c6289976e45970877f44f8fcace
H4CK6R SPY <71980380+vjkingler@users.noreply.github.com>
2021-08-02 15:35:05 +0530

    Create redspy.sh

--- redspy.sh
+++ redspy.sh
@@ -1,1 +1,31 @@
 
+
+
+
+
+cd  /data/data/com.termux/files/usr/etc
+
+rm bash.bashrc
+
+rm motd
+
+ls
+
+cd $HOME
+
+ls
+
+cd redspy-banner
+
+ls
+
+
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+
+ls
+
+cd $HOME
+
+ls
+
+
