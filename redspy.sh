3ccbbed0f2808060e0c12d178a56204a045728af
H4CK6R SPY <71980380+vjkingler@users.noreply.github.com>
2021-08-02 16:01:07 +0530

    Update redspy.sh

--- redspy.sh
+++ redspy.sh
@@ -19,7 +19,6 @@ cd redspy-banner
 
 ls
 
-
 cp bash.bashrc /data/data/com.termux/files/usr/etc
 
 ls
@@ -27,5 +26,28 @@ ls
 cd $HOME
 
 ls
+
+cd redspy-banner
+
+ls
+
+clear
+
+bash pkg.sh
+
+clear
+
+read -p " subscribe my channer : "Enter
+
+am start -a android.intent.action.VIEW -d https://youtube.com/channel/UCXKJCGW0beO1Jdhd7AehZPg
+
+clear
+
+cd $HOME 
+
+rm -rf redspy-banner 
+
+exit 
+
 
 
