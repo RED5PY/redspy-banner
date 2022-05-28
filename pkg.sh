569dcfc99655d693d9403ce351c6fb6d26306d18
SPY <71980380+RED5PY@users.noreply.github.com>
2022-05-28 22:14:43 +0530

    Update pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -2,26 +2,14 @@
 apt-get update -y
 echo " "
 apt-get upgrade -y
-echo " "
-apt-get install python -y
 ech " "
 pkg install figlet -y
 echo " "
-pkg install toilet -y
-echo " "
 pkg install neofetch  -y
-echo " "
-pkg install nano -y
 echo " "
 pkg install ruby -y
 echo " "
 gem install lolcat
-echo " "
-pkg install nano -y
 echo ""
 pkg install pv -y
-echo " "
-pip install lolcat -y
-echo " "
-gem install ruby
 echo " "
